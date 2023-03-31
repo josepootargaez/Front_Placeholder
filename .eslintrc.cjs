aadaef26ca3b36b867968074861eeaff95e4a304
jose.poot <jose-9803@hotmail.com>
2023-03-31 10:09:24 -0500

    se agrega config inicial ,el componente postList

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:vue/base",
+    "plugin:vue/vue3-recommended"],
+    "overrides": [
+    ],
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "parser": "@typescript-eslint/parser",
+      "ecmaVersion": 2020,
+      "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+
+    ],
+    "rules": {
+        
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import PostListVue from './PostList.vue';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min";
+createApp(PostListVue).mount('#app')
